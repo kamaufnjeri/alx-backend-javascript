b79609db4420e446a771b8a27f270d01a5ffb755
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-11-27 16:13:21 +0300

    ES6 basics task 0 and 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  const task = false; // Outer scope variable
+  const task2 = true;  // Outer scope variable
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task = true;  // Inner scope variable (shadows the outer variable)
+    const task2 = false;  // Inner scope variable (shadows the outer variable)
   }
   
-  return [task, task2];
+  return [task, task2];  // Returns the outer scope variables, not the ones inside the if block
 }
