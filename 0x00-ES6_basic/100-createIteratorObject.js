586e3f32af6eb22284016f1b045e289e30fe06d7
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-11-27 18:56:38 +0300

    fixing errors in 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,7 +2,7 @@ export default function createIteratorObject(report) {
   const itObject = [];
 
   for (const value of Object.values(report.allEmployees)) {
-    itObject.push(...value)
+    itObject.push(...value);
   }
 
   return itObject;
