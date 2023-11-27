2d15697e0d1c8acc5ea7c163dcf8cf240850d340
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-11-27 18:59:26 +0300

    fixing errors in 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -3,6 +3,6 @@ export default function createReportObject(employeesList) {
     allEmployees: employeesList,
     getNumberOfDepartments(employees) {
       return Object.keys(employees).length;
-    }
+    },
   };
 }
