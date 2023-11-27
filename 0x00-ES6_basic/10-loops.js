6ff92588049c3983dd01ad82d2b264c597f8613f
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-11-27 17:30:17 +0300

    ES6 tasks 10 to twelve

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (let value of array) {
-      value = appendString + value;
-    }
-  
-    return array;
+  const newArray = [];
+  for (const value of array) {
+   newArray.push(appendString + value);
   }
   
+  return newArray;
+}
