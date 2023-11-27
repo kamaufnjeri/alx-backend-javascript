c0b6b475c9bb4f0975967883b41d33438831792e
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-11-27 19:43:38 +0300

    fixing errors in 9-getFullBudget.js

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -3,10 +3,10 @@ import getBudgetObject from './7-getBudgetObject.js';
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
-    ...budget,
+    budget,
     getIncomeInDollars: (income) => `$${income}`,
     getIncomeInEuros: (income) => `${income} euros`,
-  }
+  };
 
   return fullBudget;
 }
