2730a2bc38393c7cd3690e340b22db755c1102ea
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-11-27 18:53:54 +0300

    fixing errors in 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   const myArr = [];
   for (const value of array) {
-   myArr.push(appendString + value);
+    myArr.push(appendString + value);
   }
-  
+
   return myArr;
 }
