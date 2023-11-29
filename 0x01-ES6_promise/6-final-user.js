3504ec21e125b51d4bbebf1a578b9b2a81b7cf43
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-11-29 17:38:13 +0300

    multiple promises to return status and result

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,23 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  const promises = [];
-
-  const userRes = signUpUser(firstName, lastName)
-    .then((result) => ({ status: 'fulfilled', value: result }))
-    .catch((error) => ({
-      status: 'rejected',
-      value: error,
-    }));
-
-  const photoRes = uploadPhoto(filename)
-    .then((result) => ({ status: 'fulfilled', value: result }))
-    .catch((error) => ({
-      status: 'rejected',
-      value: error,
-    }));
-
-  promises.push(userRes, photoRes);
-
-  return Promise.all(promises);
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(filename),
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : String(result.reason),
+  })));
 }
