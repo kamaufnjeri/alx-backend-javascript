e87111b4e049582c860c64e0545b9afb2ed561cb
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-11-30 21:22:26 +0300

    method that returns a new class

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,50 +1,38 @@
 export default class Car {
-    constructor(brand, motor, color) {
-        this.brand = brand;
-        this.motor = motor;
-        this.color = color;
-    }
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
 
-    // getter methods
-    get brand() {
-        return this._brand;
-    }
+  // getter methods
+  get brand() {
+    return this._brand;
+  }
 
-    get motor() {
-        return this._motor;
-    }
+  get motor() {
+    return this._motor;
+  }
 
-    get color() {
-        return this._color;
-    }
+  get color() {
+    return this._color;
+  }
 
-    // set methods
+  // set methods
 
-    set brand(value) {
-        if (typeof value === "string") {
-            this._brand = value;
-        } else {
-            throw new TypeError("Brand must be a string");
-        }
-    }
+  set brand(value) {
+    this._brand = value;
+  }
 
-    set motor(value) {
-        if (typeof value === "string") {
-            this._motor = value;
-        } else {
-            throw new TypeError("Motor must be a string");
-        }
-    }
+  set motor(value) {
+    this._motor = value;
+  }
 
-    set color(value) {
-        if (typeof value === "string") {
-            this._color = value;
-        } else {
-            throw new TypeError("Color must be a string");
-        }
-    }
+  set color(value) {
+    this._color = value;
+  }
 
-     cloneCar() {
+  cloneCar() {
     const clonedCar = new this.constructor();
     return clonedCar;
   }
