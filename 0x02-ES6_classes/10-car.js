1f8fe97674f681028f6b7b0a992d7f845a020238
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-11-30 21:12:09 +0300

    javascript classes do not support hoisting

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,50 @@
+export default class Car {
+    constructor(brand, motor, color) {
+        this.brand = brand;
+        this.motor = motor;
+        this.color = color;
+    }
+
+    // getter methods
+    get brand() {
+        return this._brand;
+    }
+
+    get motor() {
+        return this._motor;
+    }
+
+    get color() {
+        return this._color;
+    }
+
+    // set methods
+
+    set brand(value) {
+        if (typeof value === "string") {
+            this._brand = value;
+        } else {
+            throw new TypeError("Brand must be a string");
+        }
+    }
+
+    set motor(value) {
+        if (typeof value === "string") {
+            this._motor = value;
+        } else {
+            throw new TypeError("Motor must be a string");
+        }
+    }
+
+    set color(value) {
+        if (typeof value === "string") {
+            this._color = value;
+        } else {
+            throw new TypeError("Color must be a string");
+        }
+    }
+
+    cloneCar() {
+        return new Car();
+    }
+}
