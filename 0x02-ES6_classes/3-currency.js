b872daaae242b32e1ce3cb5c9e6fc0f9a4abe56d
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-11-30 16:48:48 +0300

    a class with a method

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -11,4 +11,31 @@ export default class Currency {
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
+
+  // getter methods
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  // setter methods
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be string');
+    }
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new TypeError('Code must be string');
+    }
+  }
 }
