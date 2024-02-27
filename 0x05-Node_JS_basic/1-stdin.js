b0efe4d5bd9050efd202f8ab7e6dd1a09aa86c91
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-02-27 17:24:08 +0300

    process stdout and stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,14 +2,14 @@
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim(); // Trim to remove any leading/trailing whitespace
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
   if (name) {
-    console.log(`Your name is: ${name}`);
-    console.log('This important software is now closing');
-    process.exit(); // Exit the program
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
 process.on('exit', () => {
+    process.stdout.write('This important software is now closing');
 });
