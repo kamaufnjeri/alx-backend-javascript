639233d2fa1a11e48d896325ee170d080d1b6080
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-02-27 18:52:47 +0300

    read database

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,29 +1,41 @@
-#!/usr/bin/env node
-
-/* a simple script to practice reading files */
-
 const fs = require('fs');
 
-function filterByGroup(group, dataset) {
-  return dataset.filter((entry) => entry.split(',').at(-1).toString() === group);
-}
+/** function to count student */
+function countStudents(filePath) {
+    try {
+        const data = fs.readFileSync(filePath, 'utf-8');
 
-function countStudents(path) {
-  try {
-    const data = fs.readFileSync(path, 'utf-8');
-    // get rid of the first line in the csv
-    const sData = data.toString().trim().split('\n');
-    const fullData = sData.splice(1, data.length);
-    console.log(`Number of students: ${fullData.length}`);
-    const groups = new Set(fullData.map((item) => item.split(',').at(-1)));
-    for (const group of groups) {
-      const filteredGroup = filterByGroup(group, fullData);
-      const fNames = filteredGroup.map((entry) => entry.split(',').at(0));
-      console.log(`Number of students in ${group}: ${filteredGroup.length}. List: ${fNames.join(', ')}`);
+        const lines = data.split('\n').filter(line => line.trim() !== "");
+        const studentlist =[];
+        const headers = lines[0].replace(/\r/g, "").split(",");
+
+        lines.slice(1).forEach((line) => {
+            const databaseDict = {};
+            const lineitems = line.replace(/\r/g, '').split(",");
+            lineitems.forEach((item, index) => {
+                databaseDict[headers[index]] = item;
+            });
+            studentlist.push(databaseDict);
+        });
+        console.log(`Number of students: ${studentlist.length}`);
+
+        const fieldsDict = {};
+        studentlist.forEach(student => {
+            const {firstname, field} = student;
+
+            if (!(field in fieldsDict)) {
+                fieldsDict[field] = [];
+            }
+            fieldsDict[field].push(firstname);
+        });
+        Object.keys(fieldsDict).forEach(field => {
+            const names = fieldsDict[field].join(', ');
+            console.log(`Number of students in ${field}: ${fieldsDict[field].length}. List: ${names}`);
+        })
+    } catch (err) {
+        console.log(err)
+        throw new Error('Cannot load the database');
     }
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
-}
+};
 
 module.exports = countStudents;
