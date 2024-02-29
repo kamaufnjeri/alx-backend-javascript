4df0a8cc7f821df55ba0a5cda7fc0317c98ed3cd
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-02-29 22:48:46 +0300

    done with task 3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,15 +1,31 @@
-const sinon = require('sinon');
-const Utils = require('./utils');
 const { expect } = require('chai');
+const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const bigBrother = sinon.spy(Utils);
+  it('should call Utils.calculateNumber with correct arguments', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    const totalAmount = 100;
+    const totalShipping = 20;
 
-    sendPaymentRequestToApi(100, 20);
-    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
-    bigBrother.calculateNumber.restore();
+    sendPaymentRequestToApi(totalAmount, totalShipping);
+
+    expect(calculateNumberSpy.calledOnceWith('SUM', totalAmount, totalShipping)).to.be.true;
+
+    calculateNumberSpy.restore();
+  });
+
+  it('should log the correct total', () => {
+    const consoleSpy = sinon.spy(console, 'log');
+    const totalAmount = 100;
+    const totalShipping = 20;
+    const expectedSum = totalAmount + totalShipping;
+
+    sendPaymentRequestToApi(totalAmount, totalShipping);
+
+    expect(consoleSpy.calledOnceWith(`The total is: ${expectedSum}`)).to.be.true;
+
+    consoleSpy.restore();
   });
 });
