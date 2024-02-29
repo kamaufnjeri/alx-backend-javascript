26a04a490a22cecc018a6e7304d26bf379877eb0
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-02-29 23:16:08 +0300

    done with task 8, 9, 10

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,23 +1,14 @@
-const { expect } = require('chai');
 const request = require('request');
-const server = require('./api');
+const { expect } = require('chai');
 
-describe('Index page', () => {
-  it('Correct status code?', (done) => {
-    request.get('http://localhost:7865', (error, response) => {
-      expect(response.statusCode).to.equal(200);
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
+
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
   });
-
-  it('Correct result?', (done) => {
-    request.get('http://localhost:7865', (error, response, body) => {
-      expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
-});
-
-after(() => {
-  server.close();
 });
